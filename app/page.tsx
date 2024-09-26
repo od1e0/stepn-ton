6fa8e0b2f9b139beef320ca46021ff97c29efccd
od1e0 <vladimir.zdit11@gmail.com>
2024-09-27 02:56:49 +0300

    fix timeout loadScreen

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import WebApp from "@twa-dev/sdk";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/LoadingScreen";
 
-interface UserData{
+interface UserData {
   id: number;
   first_name: string;
   last_name?: string;
@@ -15,31 +15,39 @@ interface UserData{
 
 export default function Home() {
   const [userData, setUserData] = useState<UserData | null>(null);
-
+  const [isLoading, setIsLoading] = useState(true); 
+  
   useEffect(() => {
-    if (WebApp.initDataUnsafe.user){
-      setUserData(WebApp.initDataUnsafe.user as UserData) 
+
+    if (WebApp.initDataUnsafe.user) {
+      setUserData(WebApp.initDataUnsafe.user as UserData);
     }
-  })
+
+    const timer = setTimeout(() => {
+      setIsLoading(false); 
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <main >
+    <main>
       {
-        userData ? 
+        isLoading ? 
+        <LoadingScreen /> :
         (
           <>
-          <h1 className="text-2x1 font-bold mb-4">User Data</h1>
-          <ul>
-            <li>ID: {userData.id}</li>
-            <li>First Name: {userData.first_name}</li>
-            <li>Last Name: {userData.last_name}</li>
-            <li>Username: {userData.username}</li>
-            <li>Language Code: {userData.language_code}</li>
-            <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
-          </ul>
+            <h1 className="text-2xl font-bold mb-4">User Data</h1>
+            <ul>
+              <li>ID: {userData?.id}</li>
+              <li>First Name: {userData?.first_name}</li>
+              <li>Last Name: {userData?.last_name}</li>
+              <li>Username: {userData?.username}</li>
+              <li>Language Code: {userData?.language_code}</li>
+              <li>Is Premium: {userData?.is_premium ? 'Yes' : 'No'}</li>
+            </ul>
           </>
-        ) : 
-        <LoadingScreen/>
+        )
       }
     </main>
   );
